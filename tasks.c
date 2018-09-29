297737c5e2eed13cdcd841694d19f18e027c9aea
evshary <evshary@gmail.com>
2018-09-29 16:55:11 +0800

    Use tasks structure instead of pointer. Prepare for system call implementation.

--- tasks.c
+++ tasks.c
@@ -4,9 +4,15 @@
 
 unsigned int *run_proc(unsigned int *stack);
 
-unsigned int  user_stack[USER_PROCESS][STACK_SIZE];
-unsigned int* user_stack_ptr[USER_PROCESS];
-unsigned int  current_proc_id = 0;
+struct task_type {
+    unsigned int syscall_num;
+    void *syscall_param;
+    unsigned int *user_stack_ptr;
+};
+
+unsigned int user_stack[USER_PROCESS][STACK_SIZE];
+struct task_type tasks[USER_PROCESS];
+unsigned int current_proc_id = 0;
 
 int init_process(void *proc_addr)
 {
@@ -17,7 +23,7 @@ int init_process(void *proc_addr)
      * We save 9 register and ARM save 8 register, so total is 17.
      * Ref: http://www.360doc.com/content/12/0506/11/532901_208997264.shtml
      */
-    user_stack_ptr[current_proc_id] = &user_stack[current_proc_id][STACK_SIZE - 17];
+    tasks[current_proc_id].user_stack_ptr = &user_stack[current_proc_id][STACK_SIZE - 17];
     /*
      * While lr=0xfffffffd, ARM will go to user thread mode, change sp to psp,
      * and then pop the registers (par, pc, lr, r12, r3-r0) out from psp.
@@ -32,17 +38,17 @@ int init_process(void *proc_addr)
      * Control register just change stack from msp to psp but not from
      * privileged handler mode to user thread mode.
      */
-    user_stack_ptr[current_proc_id][8] = (unsigned int)0xFFFFFFFD;
+    tasks[current_proc_id].user_stack_ptr[8] = (unsigned int)0xFFFFFFFD;
     /* It's necessary to init lr with process address first. */
-    user_stack_ptr[current_proc_id][15] = (unsigned int)proc_addr;
+    tasks[current_proc_id].user_stack_ptr[15] = (unsigned int)proc_addr;
     /* PSR Thumb bit */
-    user_stack_ptr[current_proc_id][16] = (unsigned int)0x01000000;
+    tasks[current_proc_id].user_stack_ptr[16] = (unsigned int)0x01000000;
     current_proc_id++;
     return current_proc_id;
 }
 
 void start_process(int id)
 {
-    user_stack_ptr[id - 1] = run_proc(user_stack_ptr[id - 1]);
+    tasks[id - 1].user_stack_ptr = run_proc(tasks[id - 1].user_stack_ptr);
 }
 
