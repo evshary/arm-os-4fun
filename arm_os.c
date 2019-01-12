cab016ab7f69c90a04770f489f2c4e8d29efadd9
evshary <evshary@gmail.com>
2019-01-12 16:51:34 +0800

    [PROC] Add dummy process and modify proc1 and proc2.

--- arm_os.c
+++ arm_os.c
@@ -8,37 +8,44 @@
 
 static char greet[] = "Hi, This is arm-os-4fun!\r\n";
 
+void dummy_proc(void)
+{
+    while (1);
+}
+
 void proc1(void)
 {
     int id, priority, exetime;
-    printfmt("USER: This is process 1\r\n");
+    printfmt("USER1: This is process 1\r\n");
     id = get_taskid();
-    printfmt("USER: task id=%d\r\n", id);
-    printfmt("USER: Back to process 1\r\n");
+    printfmt("USER1: task id=%d\r\n", id);
     priority = get_priority();
-    printfmt("USER: priority=%d\r\n", priority);
+    printfmt("USER1: priority=%d\r\n", priority);
     exetime = get_exetime();
-    printfmt("USER: exetime=%d\r\n", exetime);
+    printfmt("USER1: exetime=%d\r\n", exetime);
 
     while (1) {
         char buf[10];
         int ret = read(buf, 10);
         printfmt("buf[0]=%c ret=%d\r\n", buf[0], ret);
+        sleep(1);
     }
 }
 
 void proc2(void)
 {
     int id, priority, exetime;
-    printfmt("USER: This is process 2\r\n");
+    printfmt("USER2: This is process 2\r\n");
     id = get_taskid();
-    printfmt("USER: task id=%d\r\n", id);
-    printfmt("USER: Back to process 2\r\n");
+    printfmt("USER2: task id=%d\r\n", id);
     priority = get_priority();
-    printfmt("USER: priority=%d\r\n", priority);
+    printfmt("USER2: priority=%d\r\n", priority);
     exetime = get_exetime();
-    printfmt("USER: exetime=%d\r\n", exetime);
-    while (1);
+    printfmt("USER2: exetime=%d\r\n", exetime);
+    while (1) {
+        printfmt("USER2: Infinite Sleep\r\n");
+        sleep(1);
+    }
 }
 
 void main(void)
@@ -51,12 +58,18 @@ void main(void)
     printfmt(greet);
     malloc_init();
     tasks_init();
-    proc_id[0] = new_task(proc1, 1);
-    printfmt("proc_id[0]=%d\r\n", proc_id[0]);
-    proc_id[1] = new_task(proc2, 2);
+    /*
+     * We should start a dummy_proc to prevent no running process.
+     * This will cause infinite loop in kernel(Always in handler mode).
+     */
+    proc_id[0] = new_task(dummy_proc, 0);
+    /* Create your own process */
+    proc_id[1] = new_task(proc1, 1);
     printfmt("proc_id[1]=%d\r\n", proc_id[1]);
+    proc_id[2] = new_task(proc2, 2);
+    printfmt("proc_id[2]=%d\r\n", proc_id[2]);
     while (1) {
         tasks_scheduler();
     }
-    while (1);
+    while (1); /* You should not enter here. */
 }
