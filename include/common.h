bac03c2c07880a9cb49413fb34c19a17b587f898
evshary <evshary@gmail.com>
2019-01-20 16:02:20 +0800

    [Library] Add memset function.

--- include/common.h
+++ include/common.h
@@ -2,5 +2,6 @@
 #define __COMMON_H__
 
 void memcpy(void *dst, const void *src, unsigned int num);
+void memset(void *ptr, int value, unsigned int num);
 
 #endif /*__COMMON_H__*/
--- library/common.c
+++ library/common.c
@@ -5,3 +5,11 @@ void memcpy(void *dst, const void *src, unsigned int num)
         ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
     }
 }
+
+void memset(void *ptr, int value, unsigned int num)
+{
+    unsigned int i;
+    for (i = 0; i < num; i++) {
+        ((unsigned char *)ptr)[i] = (unsigned char)value;
+    }
+}
